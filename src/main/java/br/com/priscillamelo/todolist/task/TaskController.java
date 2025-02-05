922cf4f39fa3d41b65ab422338ec86b2b1922f01
Priscilla Melo <priscillamelo73@gmail.com>
2025-02-04 22:50:21 -0300

    feat: validate task date

--- src/main/java/br/com/priscillamelo/todolist/task/TaskController.java
+++ src/main/java/br/com/priscillamelo/todolist/task/TaskController.java
@@ -1,9 +1,11 @@
 package br.com.priscillamelo.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,10 +22,18 @@ public class TaskController {
 
     @PostMapping("/create-task")
     public ResponseEntity<String> createTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        UUID idUser = (UUID) request.getAttribute("isUser");
+        UUID idUser = (UUID) request.getAttribute("idUser");
         taskModel.setIdUser(idUser);
-        var task = this.taskRepository.save(taskModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(task.toString());
+        LocalDateTime currentDate = LocalDateTime.now();
+        boolean validStartDate = currentDate.isBefore(taskModel.getStartAt());
+        boolean validEndDate = currentDate.isBefore(taskModel.getEndAt());
+        boolean validDate = taskModel.getEndAt().isAfter(taskModel.getStartAt());
+        if ((validStartDate) && (validEndDate) && (validDate)) {
+            var task = this.taskRepository.save(taskModel);
+            return ResponseEntity.status(HttpStatus.CREATED).body(task.toString());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                .body("Data inválida, confira as datas de início e término da tarefa!");
     }
 
 }
